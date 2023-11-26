6886ac265a4461c568dc6a9e7c12d6f22d9e61eb
Gyeony95 <rlg1133@naver.com>
2023-11-26 18:14:24 +0900

    feat : 파이어페이스 추가 및 dot env 불러올수 있게 처리

--- lib/dev_main.dart
+++ lib/dev_main.dart
@@ -1,14 +1,20 @@
 
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:imjanger/di/get_it_injector.dart';
 import 'package:imjanger/enviroment.dart';
+import 'package:imjanger/firebase_options_dev.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await dotenv.load();
+  await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(
+    name: dotenv.env['FIREBASE_KEY_DEV'],
+    options: DevFirebaseOptions.currentPlatform,
+  );
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
